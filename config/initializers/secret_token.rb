69d05356f46ca1aeacdb32c9fd6d0a85df4b93a0
Tomas Carnecky <tomas.carnecky@gmail.com>
2011-06-25 10:58:33 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lcem::Application.config.secret_token = '3eb617a421c5379c4ee52e3d222a6fde71e86f1e94b95dd8e547596d5445595ec0a5f74b0d8b1013e48366d8a85e18a06ecf5a922585d30172faac8e04031140'
